844fd67b04f1ee0714673eb73bbd356422acdfb6
jason <jason@interactivelabs.co>
2014-07-15 15:24:03 +0800

    SKNode clean up children

---
+++ RSKit/SKNode+ChildrenClean.h
@@ -0,0 +1,15 @@
+//
+//  SKNode+TPNodeClean.h
+//  Pods
+//
+//  Created by jason.huang on 7/15/14.
+//
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SKNode (ChildrenClean)
+
+- (void)cleanUpChildrenAndRemove;
+
+@end
---
+++ RSKit/SKNode+ChildrenClean.m
@@ -0,0 +1,22 @@
+//
+//  SKNode+TPNodeClean.m
+//  Pods
+//
+//  Created by jason.huang on 7/15/14.
+//
+//
+
+#import "SKNode+ChildrenClean.h"
+
+@implementation SKNode (ChildrenClean)
+
+- (void)cleanUpChildrenAndRemove
+{
+  for (SKNode *child in self.children) {
+    [child cleanUpChildrenAndRemove];
+  }
+  [self removeAllActions];
+  [self removeFromParent];
+}
+
+@end
